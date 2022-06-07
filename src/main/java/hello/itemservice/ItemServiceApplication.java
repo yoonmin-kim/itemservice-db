f53d8c0cf5461cd82f9d2c8eec1648b02a3d855d
yoonmin-kim <rladbsals23@gmail.com>
2022-06-07 23:11:00 +0900

    feat : 데이터 접근 기술 - MyBatis

--- src/main/java/hello/itemservice/ItemServiceApplication.java
+++ src/main/java/hello/itemservice/ItemServiceApplication.java
@@ -7,13 +7,15 @@ import org.springframework.context.annotation.Import;
 import org.springframework.context.annotation.Profile;
 
 import hello.itemservice.config.JdbcTemplateV3Config;
+import hello.itemservice.config.MyBatisConfig;
 import hello.itemservice.repository.ItemRepository;
 
 
 // @Import(MemoryConfig.class)
 // @Import(JdbcTemplateV1Config.class)
 // @Import(JdbcTemplateV2Config.class)
-@Import(JdbcTemplateV3Config.class)
+// @Import(JdbcTemplateV3Config.class)
+@Import(MyBatisConfig.class)
 @SpringBootApplication(scanBasePackages = "hello.itemservice.web")
 public class ItemServiceApplication {
 
---
+++ src/main/java/hello/itemservice/config/MyBatisConfig.java
@@ -0,0 +1,30 @@
+package hello.itemservice.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+import hello.itemservice.repository.ItemRepository;
+import hello.itemservice.repository.memory.MemoryItemRepository;
+import hello.itemservice.repository.mybatis.ItemMapper;
+import hello.itemservice.repository.mybatis.MyBatisItemRepository;
+import hello.itemservice.service.ItemService;
+import hello.itemservice.service.ItemServiceV1;
+import lombok.RequiredArgsConstructor;
+
+@Configuration
+@RequiredArgsConstructor
+public class MyBatisConfig {
+
+    private final ItemMapper itemMapper;
+
+    @Bean
+    public ItemService itemService() {
+        return new ItemServiceV1(itemRepository());
+    }
+
+    @Bean
+    public ItemRepository itemRepository() {
+        return new MyBatisItemRepository(itemMapper);
+    }
+
+}
